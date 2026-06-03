a1597e86c4176eb7d6959adf4365efc71638455d
Coskun <coskun.erol03@gmail.com>
2026-06-03 05:01:59 +0300

    On hit knockback , heavy knockback added.

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Entity : MonoBehaviour
 {
@@ -18,7 +20,10 @@ public class Entity : MonoBehaviour
     public int facingDir { get; private set; } = 1;
     public bool groundDetected { get; private set; }
     public bool wallDetected { get; private set; }
-    
+
+    // Condition variables
+    private bool isKnocked;
+    private Coroutine knockbackCo;
 
 
     protected virtual void Awake() {
@@ -41,7 +46,30 @@ public class Entity : MonoBehaviour
         stateMachine.currentState.AnimationTrigger();
     }
 
+    public void ReciveKnockback(Vector2 knockback, float duration) {
+
+        if (knockbackCo != null)
+            StopCoroutine(knockbackCo);
+
+        knockbackCo = StartCoroutine(KnockbackCo(knockback, duration));
+    }
+
+    private IEnumerator KnockbackCo(Vector2 knockbeack, float duration) {
+
+        isKnocked = true;
+        rb.linearVelocity = knockbeack;
+
+        yield return new WaitForSeconds(duration);
+
+        rb.linearVelocity = Vector2.zero;
+        isKnocked = false;
+    }
+
     public void SetVelocity(float xVelocity, float yVelocity) {
+        if(isKnocked) {
+            return;
+        }
+        
         rb.linearVelocity = new Vector2(xVelocity, yVelocity);
         HandleFlip(xVelocity);
     }
--- Assets/Scripts/Entity_Health.cs
+++ Assets/Scripts/Entity_Health.cs
@@ -5,13 +5,25 @@ using UnityEngine.Rendering;
 public class Entity_Health : MonoBehaviour
 {
     private EntityVFX entityVFX;
+    private Entity entity;
 
+    [SerializeField] protected float currentHp;
     [SerializeField] protected float maxHp = 100;
     [SerializeField] protected bool isDead;
 
+    [Header("On Damage Knockback")]
+    [SerializeField] private Vector2 knocbackPower = new Vector2(1.5f , 2.5f);
+    [SerializeField] private Vector2 heavyKnockbackPower = new Vector2(7, 7);
+    [SerializeField] private float knocbackDuration = .2f;
+    [SerializeField] private float heavyKnockbackDuration = .5f;
+    [Header("On Heavy Damage")]
+    [SerializeField] private float heavyKnockbackThreshold = .3f;
 
     protected virtual void Awake() {
         entityVFX = GetComponent<EntityVFX>();
+        entity = GetComponent<Entity>();
+
+        currentHp = maxHp;
     }
 
 
@@ -20,6 +32,10 @@ public class Entity_Health : MonoBehaviour
         if (isDead)
             return;
 
+        Vector2 knockback = CalculateKnockback(damage,damageDealer);
+        float duration = CalculationDuration(damage);
+
+        entity?.ReciveKnockback(knockback, duration);
         entityVFX?.PlayOnDamageVfx();
         //burada kullanýlan '?' bir null check'tir. eðer boþ deðer varsa hata fýrlatmamasý için. 
 
@@ -28,9 +44,9 @@ public class Entity_Health : MonoBehaviour
 
     protected void ReduceHp(float damage) {
 
-        maxHp -= damage;
+        currentHp -= damage;
 
-        if(maxHp < 0) {
+        if(currentHp < 0) {
             Die();
         }
     }
@@ -39,4 +55,18 @@ public class Entity_Health : MonoBehaviour
         isDead = true;
         Debug.Log("Entity died!!!");
     }
+
+    private Vector2 CalculateKnockback(float damage , Transform damageDealer) {
+        int direction = transform.position.x > damageDealer.position.x ? 1 : -1;
+
+        Vector2 knocback = IsHeavyDamage(damage) ? heavyKnockbackPower : knocbackPower;
+        knocback.x *= direction;
+        
+        return knocback;
+    }
+
+    private float CalculationDuration(float damage) => IsHeavyDamage(damage) ? heavyKnockbackDuration : knocbackDuration;
+
+    private bool IsHeavyDamage(float damage) => damage / maxHp > heavyKnockbackThreshold;
+
 }
